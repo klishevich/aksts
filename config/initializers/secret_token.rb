868c0852688bff5ed3a8437d76b4311065dd16de
klisha <mans1@ngs.ru>
2013-09-07 22:23:35 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aksts::Application.config.secret_token = 'e69199c1a0c4f5840c328d948957e8bca42db17d28b476b4eff28c35c6f4fea45c61d7454c5b00d4db73f954f550ba0c5931b1349a94f90e604dd4347451f88e'
